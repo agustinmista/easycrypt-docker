42093223f6afcb248372f7cc374ade734683960d
Agustin Mista <mista@chalmers.se>
2022-11-15 20:06:19 +0100

    Added some minor consistency tweak here and there

--- command.sh
+++ command.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
-docker run --name easycrypt -it --entrypoint "/usr/bin/bash" -v "$PWD:/home/easycrypt/workdir" agustinmista/easycrypt ||
-  docker start easycrypt && docker exec -it easycrypt /usr/bin/bash
+#!/bin/bash
+
+docker run --name easycrypt -it --entrypoint /bin/bash -v $PWD:/home/easycrypt/workdir agustinmista/easycrypt ||
+  docker start easycrypt && docker exec -it easycrypt /bin/bash
