e6d7adac2b376fd9334f7a02701cec5da327503a
Matthías Páll Gissurarson <mpg@mpg.is>
2022-11-16 14:44:21 +0100

    Make emacs work in gui mode

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker run --name easycrypt -it --entrypoint "/usr/bin/bash" -v "$PWD:/home/easycrypt/workdir" agustinmista/easycrypt ||
-  docker start easycrypt && docker exec -it easycrypt /usr/bin/bash
+docker run --name easycrypt -e DISPLAY -it --entrypoint "/usr/bin/bash" -v /tmp/.X11-unix:/tmp/.X11-unix -v "$PWD:/home/easycrypt/workdir" agustinmista/easycrypt ||
+ docker start easycrypt && docker exec -e DISPLAY -it easycrypt /usr/bin/bash
